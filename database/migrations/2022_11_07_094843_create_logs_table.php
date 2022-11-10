e52a4263c2c61352bd5f2569d92304aed5df5aad
bari hanane <hn_bari@hotmail.com>
2022-11-10 08:11:06 +0100

    spatie

--- database/migrations/2022_11_07_094843_create_logs_table.php
+++ database/migrations/2022_11_07_094843_create_logs_table.php
@@ -3,10 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
-    /**
+    /*
      * Run the migrations.
      *
      * @return void
@@ -23,7 +22,6 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
